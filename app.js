43327ba21a4202283702fad0e087ed356a0dcc1b
Hitendra Mali <39190812+h1t3ndr4@users.noreply.github.com>
2025-08-15 21:27:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require("express");
 const archiver = require("archiver");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Add JSON parsing middleware
 app.use(express.json());
